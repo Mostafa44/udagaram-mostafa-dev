5f75647d6473c8daf9bb5f57282114754bfea928
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-02-12 21:17:56 +0200

    add the files for the feed end point

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -6,42 +6,42 @@ import { IndexRouter } from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import { config } from './config/config';
-import { V0_FEED_MODELS } from './controllers/v0/model.index';
-import * as dotenv from 'dotenv';
+import { V0_FEED_MODELS, V0_USER_MODELS } from './controllers/v0/model.index';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 (async () => {
-    await sequelize.addModels(V0_FEED_MODELS);
-    //await sequelize.addModels(V0_USER_MODELS);
-    await sequelize.sync();
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.sync();
 
-    const app = express();
-    const port = process.env.PORT || 8081;
-    console.log("here it is ");
-    console.log(process.env.POSTGRES_USERNAME);
-    app.use(bodyParser.json());
+  const app = express();
+  const port = process.env.PORT || 8080;
+  console.log("here it is ");
+  console.log(process.env.POSTGRES_USERNAME);
+  app.use(bodyParser.json());
 
-    app.use(cors({
-        allowedHeaders: [
-            'Origin', 'X-Requested-With',
-            'Content-Type', 'Accept',
-            'X-Access-Token', 'Authorization',
-        ],
-        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-        origin: config.url,
-    }));
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: config.url,
+  }));
 
-    app.use('/api/v0/', IndexRouter);
+  app.use('/api/v0/', IndexRouter);
 
-    // Root URI call
-    app.get('/', async (req, res) => {
-        res.send('/api/v0/');
-    });
+  // Root URI call
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
+  });
 
 
-    // Start the Server
-    app.listen(port, () => {
-        console.log(`feed-api-server running ${config.url}`);
-        console.log(`press CTRL+C to stop server`);
-    });
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running ${config.url}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();

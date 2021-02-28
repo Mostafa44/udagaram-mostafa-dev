a6519fde44ce71163a7b5b9de53dc27d50acfacb
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-02-28 13:02:58 +0200

    update the git ignore files so that it exclude the environment variables related files

---
+++ udagram-api-feed/src/server.ts
@@ -0,0 +1,47 @@
+import cors from 'cors';
+import express from 'express';
+import { sequelize } from './sequelize';
+
+import { IndexRouter } from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import { config } from './config/config';
+import { V0_FEED_MODELS } from './controllers/v0/model.index';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+(async () => {
+    await sequelize.addModels(V0_FEED_MODELS);
+    //await sequelize.addModels(V0_USER_MODELS);
+    await sequelize.sync();
+
+    const app = express();
+    const port = process.env.PORT || 8081;
+    console.log("here it is ");
+    console.log(process.env.POSTGRES_USERNAME);
+    app.use(bodyParser.json());
+
+    app.use(cors({
+        allowedHeaders: [
+            'Origin', 'X-Requested-With',
+            'Content-Type', 'Accept',
+            'X-Access-Token', 'Authorization',
+        ],
+        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+        origin: config.url,
+    }));
+
+    app.use('/api/v0/', IndexRouter);
+
+    // Root URI call
+    app.get('/', async (req, res) => {
+        res.send('/api/v0/');
+    });
+
+
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`feed-api-server running ${config.url}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+})();
